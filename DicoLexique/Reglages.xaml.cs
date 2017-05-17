5378a46aa80b826d97099bfd6e396160448bfe4e
Le1cOol <vincent.denis.1993@gmail.com>
2017-05-17 14:06:41 +0200

    Settings

--- DicoLexique/Reglages.xaml.cs
+++ DicoLexique/Reglages.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DicoLexique.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,9 +23,51 @@ namespace DicoLexique
     /// </summary>
     public sealed partial class Reglages : Page
     {
-        public Reglages()
+        private Settings settings = new Settings();
+
+        public Reglages(Settings s)
         {
+            this.settings = s;
             this.InitializeComponent();
+        }
+
+        private void reglageMicroBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.reglageMicroBox.IsChecked == true)
+            {
+                this.settings.Micro = true;
+            }
+            else
+            {
+                this.settings.Micro = false;
+            }
+        }
+
+        private void reglageBrailleBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.reglageBrailleBox.IsChecked == true)
+            {
+                this.settings.ModeAveugle = true;
+                this.reglageMicroBox.IsChecked = true;
+                this.reglageMicroBox.IsEnabled = false;
+            }
+            else
+            {
+                this.settings.ModeAveugle = false;
+                this.reglageMicroBox.IsEnabled = true;
+            }
+        }
+
+        private void reglageSonBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.reglageSonBox.IsChecked == true)
+            {
+                this.settings.Sound = true;
+            }
+            else
+            {
+                this.settings.Sound = false;
+            }
         }
     }
 }
